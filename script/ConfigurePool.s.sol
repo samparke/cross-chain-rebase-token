4b4c3ede6d81d4515fa0f533ac6e4ff9458bce8b
Samuel Parke <samueljparke@gmail.com>
2025-08-04 19:10:20 +0100

    configure pool script

---
+++ script/ConfigurePool.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {TokenPool} from "ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {RateLimiter} from "ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+
+contract ConfigurePoolScript is Script {
+    function run(address localPool, uint64 remoteChainSelector, address remotePool, address remoteToken) public {
+        vm.startBroadcast();
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            allowed: true,
+            remotePoolAddress: abi.encode(address(remotePool)),
+            remoteTokenAddress: abi.encode(address(remoteToken)),
+            outboundRateLimiterConfig: RateLimiter.Config({isEnabled: false, capacity: 0, rate: 0}),
+            inboundRateLimiterConfig: RateLimiter.Config({isEnabled: false, capacity: 0, rate: 0})
+        });
+        TokenPool(localPool).applyChainUpdates(chainsToAdd);
+    }
+}
